36c2c0629f9d5f50172177ba1344bd9252523e1e
ivanheral <ivanhernandezal@gmail.com>
2020-01-24 20:34:05 +0100

    update

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+const presets = [
+    ["@babel/env", {
+        targets: {
+            "chrome": "72"
+        },
+        corejs: 3,
+        useBuiltIns: "usage"
+    }]
+];
+
+module.exports = {
+    presets
+};
